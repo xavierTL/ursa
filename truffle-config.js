fba2a558dbc876a5e2a7775a4366e9adc4b1112c
Xavier Bruggen <xav@Xaviers-Air.default>
2019-01-21 22:23:28 +0000

    gutted old ui, starting over

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraApikey = '540b9d8e15344109bafbcebb786f6efe';
-let mnemonic = require('./mnemonic');
 
 module.exports = {
   networks: {
@@ -8,17 +7,17 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          mnemonic,
-          `https://ropsten.infura.io/v3/${infuraApikey}`
-        );
-      },
-      network_id: 3,
-      gas: 7000000, // default = 4712388
-      gasPrice: 2000000000 // default = 100 gwei = 100000000000
     }
+    // ropsten: {
+    //   provider: function() {
+    //     return new HDWalletProvider(
+    //       mnemonic,
+    //       `https://ropsten.infura.io/v3/${infuraApikey}`
+    //     );
+    //   },
+    //   network_id: 3,
+    //   gas: 7000000, // default = 4712388
+    //   gasPrice: 2000000000 // default = 100 gwei = 100000000000
+    // }
   }
 };
