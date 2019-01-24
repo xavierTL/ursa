75ea6a8a92d03c0f7906f01e48add5dd48a79b5d
Xavier Bruggen <xav@Xaviers-Air.default>
2019-01-24 18:03:39 +0000

    progress bar working with updates to title

--- client/src/components/NewElectionForm.jsx
+++ client/src/components/NewElectionForm.jsx
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import { ProgressBar } from 'react-bootstrap';
+import {
+  ProgressBar,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  HelpBlock
+} from 'react-bootstrap';
 import NewElectionHeader from './NewElectionHeader';
 
 class NewElectionForm extends Component {
   state = {
-    now: 3
+    title: '',
+    titleDone: false
   };
   render() {
-    const { now } = this.state;
+    const { title, titleDone } = this.state;
+    let now = [titleDone].filter(x => x === true).length * 20;
     return (
       <div>
         <NewElectionHeader />
         <ProgressBar now={now} active label={`${now}%`} />
+        <form>
+          <FormGroup
+            controlId="formBasicText"
+            validationState={this.getValidationState('title')}
+          >
+            <ControlLabel>Title.</ControlLabel>
+            <FormControl
+              type="text"
+              value={title}
+              placeholder="e.g. 'Who should cook dinner tonight?'"
+              onChange={this.handleTitleChange}
+            />
+            <FormControl.Feedback />
+            <HelpBlock>Must be at least 10 characters.</HelpBlock>
+          </FormGroup>
+        </form>
       </div>
     );
   }
@@ -19,6 +43,22 @@ class NewElectionForm extends Component {
     const { methods } = this.props.drizzle.contracts.ElectionManager;
     console.log(await methods.smokeTest().call());
   };
+
+  handleTitleChange = e => {
+    this.setState({ title: e.target.value }, () => {
+      const { title } = this.state;
+      if (title.length > 10) {
+        this.setState({ titleDone: true });
+      } else {
+        this.setState({ titleDone: false });
+      }
+    });
+  };
+
+  getValidationState(data) {
+    const length = this.state[data].length;
+    return length > 10 ? 'success' : 'warning';
+  }
 }
 
 export default NewElectionForm;
