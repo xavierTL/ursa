1724686d648854b55f90e344ac4d1173e2a290fe
Xavier Bruggen <xav@Xaviers-Air.default>
2019-01-24 16:54:48 +0000

    new election form styled and logo added

--- client/src/components/NewElectionForm.jsx
+++ client/src/components/NewElectionForm.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { ProgressBar } from 'react-bootstrap';
+import { ProgressBar, Jumbotron, Image } from 'react-bootstrap';
 
 class NewElectionForm extends Component {
   state = {
-    now: 80
+    now: 3
   };
   render() {
     const { now } = this.state;
     return (
       <div>
-        <ProgressBar now={80} active label={`${now}%`} />
+        <Jumbotron className="new-election-jumbo">
+          <div className="new-election-header">
+            <div>
+              <h1 className="new-election-title">Your new election.</h1>
+            </div>
+            <div>
+              <Image
+                src="https://image.flaticon.com/icons/svg/1346/1346580.svg"
+                className="icon"
+              />
+            </div>
+          </div>
+        </Jumbotron>
+        <ProgressBar now={now} active label={`${now}%`} />
       </div>
     );
   }
