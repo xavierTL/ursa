90a5d59bee8b972029128b061711569c75c6234b
Xavier Bruggen <xav@Xaviers-Air.default>
2019-01-24 10:30:50 +0000

    bootstrap hooked up to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import UrsaHeader from './components/UrsaHeader';
 
 class App extends Component {
+  state = {
+    user: 'not connected to metamask'
+  };
   render() {
+    const { user } = this.state;
     console.log(this.props);
     return (
       <div className="App">
-        <h1>xavs ursa</h1>
+        <UrsaHeader user={user} />
       </div>
     );
   }
---
+++ client/src/components/UrsaHeader.jsx
@@ -0,0 +1,25 @@
+import { Navbar } from 'react-bootstrap';
+import React, { Component } from 'react';
+
+class UrsaHeader extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#home">Ursa</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Text pullRight>
+            Signed in as: <Navbar.Link href="#">{this.props.user}</Navbar.Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default UrsaHeader;
