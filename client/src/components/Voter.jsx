b8da6eeb1f596c294877fc8211e6c931ab068864
Xavier Bruggen <xav@Xaviers-Air.default>
2019-02-27 17:39:08 +0000

    vote buttons added

--- client/src/components/Voter.jsx
+++ client/src/components/Voter.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Button, Alert, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Voter extends Component {
   state = {
-    voted: false
+    voted: false,
+    selection: ''
   };
   render() {
     const { voted } = this.state;
     const { candidates } = this.props;
     return (
       <>
+        <ListGroup>
+          {candidates.map((cand, i) => (
+            <ListGroupItem key={i}>
+              {cand[1][1]}
+              <Button key={i} onClick={() => console.log(cand)}>
+                Select
+              </Button>
+            </ListGroupItem>
+          ))}
+        </ListGroup>
         {!voted ? (
           <>
             <Button onClick={() => this.castVote()}>Submit Vote</Button>
@@ -24,7 +35,7 @@ class Voter extends Component {
   }
 
   componentDidMount() {
-    this.castVote();
+    // this.castVote();
   }
 
   castVote = async candId => {
